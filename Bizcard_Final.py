7cb3ce12110142892f766308c5203852aa9bc22b
Abdulsalam-47 <167304855+Abdulsalam-47@users.noreply.github.com>
2024-09-03 09:55:29 +0530

    Bizcard_Final.py

--- Bizcard_Final.py
+++ Bizcard_Final.py
@@ -9,10 +9,16 @@ import re
 
 # Function to convert image to text
 def img_to_txt(path):
+    
+    #Open the image
     input_img = Image.open(path)
+    #converting the image to an array
     img_array = np.array(input_img)
+    #initialinzing the OCR
     reader = easyocr.Reader(['en'])
+    #Performing text recognition
     txt = reader.readtext(img_array, detail=0)
+    #Returning Result
     return txt, input_img
 
 # Function to extract text
@@ -30,22 +36,30 @@ def extracted_txt(txts):
     }
 
     if txts:
+        #extd_dict["NAME"].append(txts[0]): Assumes the first text string in the list (txts[0]) is the name and adds it to the "NAME" field.
         extd_dict["NAME"].append(txts[0])
+        #extd_dict["DESIGNATION"].append(txts[1]): Assumes the second text string (txts[1]) is the designation and adds it to the "DESIGNATION" field
         extd_dict["DESIGNATION"].append(txts[1])
         
+        #for i in range(2, len(txts)):: Loops through the remaining text strings starting from index 2.
         for i in range(2, len(txts)):
+            #Checks if the text starts with + or if it contains digits with -, assuming it is a contact number.Combines multiple contact numbers if present.
+
             if txts[i].startswith("+") or (txts[i].replace("-", "").isdigit() and '-' in txts[i]):
                 if extd_dict["CONTACT"]:
                     extd_dict["CONTACT"][0] += " & " + txts[i]
                 else:
                     extd_dict["CONTACT"].append(txts[i])
+            #Checks if the text contains @ and .com, assuming it is an email address.
             elif "@" in txts[i] and ".com" in txts[i]:
                 extd_dict["EMAIL"].append(txts[i])
+            #Checks if the text contains "www". If not formatted correctly, it fixes the format and adds it as a website.
             elif "www" in txts[i].lower():
                 website = txts[i].lower()
                 if "www." not in website:
                     website = website.replace("www", "www.")
                 extd_dict["WEBSITE"].append(website)
+            #Checks for the presence of "Tamil Nadu" or "TamilNadu" and assigns it as the state
             elif "Tamil Nadu" in txts[i] or "TamilNadu" in txts[i]:
                 extd_dict["STATE"].append("Tamil Nadu")
                 # Extract pincode if present in the same string
@@ -54,13 +68,15 @@ def extracted_txt(txts):
                     extd_dict["PINCODE"].append(pincode_match.group())
             elif re.match(r'^\d{6}$', txts[i]):
                 extd_dict["PINCODE"].append(txts[i])
+            #If the text starts with a letter, it is assumed to be the company name.
             elif re.match(r'^[A-Za-z]', txts[i]):
                 extd_dict["COMPANY NAME"].append(txts[i].upper())
             else:
-                # Remove unnecessary spaces and replace semicolons with commas
+                # All other text is considered as address, Remove unnecessary spaces and replace semicolons with commas
                 address = txts[i].strip().replace(';', ',')
                 extd_dict["ADDRESS"].append(address)
 
+        #Joins multiple entries for each field into a single string separated by spaces. If a field has no data, it assigns "NA" as the default value.
         for key, value in extd_dict.items():
             extd_dict[key] = [" ".join(value)] if value else ["NA"]
 
@@ -88,7 +104,8 @@ if select == "Home":
     st.write('### The main purpose of Bizcard is to automate the process of extracting key details from business card images, such as the name, designation, company, contact information, and other relevant data. By leveraging the power of OCR (Optical Character Recognition) provided by EasyOCR, Bizcard is able to extract text from the images.')
 
     st.write('### :blue[Technologies Used]')
-    st.write('### [Python]  [Streamlit] [EasyOCR]  [PIL(Python Imaging Library)]  [MySQL]')
+    st.write('### :white[Python]  :white[Streamlit] :white[EasyOCR]  :white[PIL(Python Imaging Library)]  :white[MySQL]')
+
 
 elif select == "Upload Data":
     img = st.file_uploader("Upload the Image", type=["png", "jpg", "jpeg"])
@@ -101,10 +118,18 @@ elif select == "Upload Data":
         if text_dict:
             st.success("TEXT IS EXTRACTED SUCCESSFULLY")
 
-        df = pd.DataFrame(text_dict)
-        st.dataframe(df)
+        method = st.radio("Select the Method", ["None", "Preview"])
 
-        if st.button("Save"):
+        # Display DataFrame only if "Preview" is selected
+        if method == "Preview":
+            df = pd.DataFrame(text_dict)
+            st.write("### Preview of Extracted Data")
+            st.dataframe(df)
+            # Note about modifying data
+            st.markdown("**Please note:** If the data is not properly placed or some values are missing, please add or change the missed values in the 'Modify Data' tab.")
+
+        if st.button("Save") and method == "Preview":
+            # Connect to the database
             mydb = mysql.connector.connect(
                 host="127.0.0.1",
                 port="3306",
@@ -114,53 +139,45 @@ elif select == "Upload Data":
             )
             cursor = mydb.cursor()
 
-            # Table Creation
-            create_query = '''
-                CREATE TABLE IF NOT EXISTS bizcard_info (
-                    name VARCHAR(225),
-                    designation VARCHAR(225),
-                    company_name VARCHAR(225),
-                    contact VARCHAR(225),
-                    email VARCHAR(225),
-                    website TEXT,
-                    address TEXT,
-                    state VARCHAR(225),
-                    pincode VARCHAR(225)
-                )
+            # Check if record already exists
+            check_query = '''
+                SELECT * FROM bizcard_info
+                WHERE name = %s AND designation = %s AND company_name = %s
             '''
-            cursor.execute(create_query)
-            mydb.commit()
+            cursor.execute(check_query, (text_dict["NAME"][0], text_dict["DESIGNATION"][0], text_dict["COMPANY NAME"][0]))
+            result = cursor.fetchone()
 
-            # Insert Query
-            insert_query = '''
-                INSERT INTO bizcard_info (
-                    name, designation, company_name, contact, email, website, address, state, pincode
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-            '''
-            cursor.execute(insert_query, df.values.tolist()[0])
-            mydb.commit()
-            st.success("SAVED SUCCESSFULLY")
+            if result:
+                st.warning("A record with the same name, designation, and company name already exists. No new record was added.")
+            else:
+                # Table Creation
+                create_query = '''
+                    CREATE TABLE IF NOT EXISTS bizcard_info (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        name VARCHAR(225),
+                        designation VARCHAR(225),
+                        company_name VARCHAR(225),
+                        contact VARCHAR(225),
+                        email VARCHAR(225),
+                        website TEXT,
+                        address TEXT,
+                        state VARCHAR(225),
+                        pincode VARCHAR(225)
+                    )
+                '''
+                cursor.execute(create_query)
+                mydb.commit()
 
-    method = st.radio("Select the Method", ["None", "Preview"])
+                # Insert Query
+                insert_query = '''
+                    INSERT INTO bizcard_info (
+                        name, designation, company_name, contact, email, website, address, state, pincode
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                '''
+                cursor.execute(insert_query, df.values.tolist()[0])
+                mydb.commit()
+                st.success("SAVED SUCCESSFULLY")
 
-    if method == "Preview":
-        mydb = mysql.connector.connect(
-            host="127.0.0.1",
-            port="3306",
-            user="root",
-            password="root",
-            database="bizcardx"
-        )
-        cursor = mydb.cursor()
-
-        # Select query
-        select_query = "SELECT * FROM bizcard_info"
-        cursor.execute(select_query)
-        table = cursor.fetchall()
-        mydb.commit()
-
-        table_df = pd.DataFrame(table, columns=("NAME", "DESIGNATION", "COMPANY_NAME", "CONTACT", "EMAIL", "WEBSITE", "ADDRESS", "STATE", "PINCODE"))
-        st.dataframe(table_df)
 
 
 elif select == "Modify Data":
@@ -263,6 +280,5 @@ elif select == "Delete Data":
             st.success(f"{selected_name} has been deleted successfully.")
     else:
         st.warning("Please confirm the deletion.")
-
 
 
